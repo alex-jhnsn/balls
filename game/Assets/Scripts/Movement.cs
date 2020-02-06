bc0e820fb9133e27baa799df0e65bd2796bab9fd
Alex Johnson <alextheviking@hotmail.co.uk>
2020-02-06 16:28:56 +0000

    Adds movement script

--- game/Assets/Scripts/Movement.cs
+++ game/Assets/Scripts/Movement.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    public float speed;
+    Rigidbody rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float moveHorizontal = Input.GetAxis ("Horizontal");
+        float moveVertical = Input.GetAxis ("Vertical");
+
+        Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce (movement * speed);
     }
 }
