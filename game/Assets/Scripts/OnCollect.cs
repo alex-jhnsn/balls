50b528a93cc6a4fb827b524469d273286a2aa4d2
feifei520520 <60659415+feifei520520@users.noreply.github.com>
2020-02-07 00:54:39 +0800

    Coin does the spin

--- game/Assets/Scripts/OnCollect.cs
+++ game/Assets/Scripts/OnCollect.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class OnCollect : MonoBehaviour
 {
+    public float speed;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,13 +14,14 @@ public class OnCollect : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(Vector3.forward * (Time.deltaTime *speed));
     }
 
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.name == "Ball")
         {
+            other.gameObject.GetComponent<PointsManager>().givePoint();
             Destroy(gameObject);
         }
     }
